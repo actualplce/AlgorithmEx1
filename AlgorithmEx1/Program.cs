17e4cdd51a49f3e23e726e6f4d6432d62328504a
actualplce <acutalplce@hanmail.net>
2021-08-06 21:38:10 +0900

    초기 커밋.

---
+++ AlgorithmEx1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AlgorithmEx1
+{
+    class Program
+    {
+        static int[] Main(string[,] places)
+        {
+
+           
+
+
+
+
+            int[] answer = new int[] { };
+ 
+            return answer;
+        }
+    }
+}
