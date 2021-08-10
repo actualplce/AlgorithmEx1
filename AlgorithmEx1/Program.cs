23763dd5c07615bf25cfc8702efe75f45361399d
asrock <asrock@YBK>
2021-08-10 12:34:24 +0900

    test

--- AlgorithmEx1/Program.cs
+++ AlgorithmEx1/Program.cs
@@ -4,17 +4,43 @@ namespace AlgorithmEx1
 {
     class Program
     {
-        static int[] Main(string[,] places)
+        static void Main(string[] args)
         {
 
-           
+            string[,] places = new string[,]
+            {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, 
+            {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"}, 
+            {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, 
+            {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, 
+            {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}
+            } ;
+
+            int[] answer = new int[5] {0,0,0,0,0 };
+
+            var a = places[0, 0][0];
+            var aColumn = places[0, 0][1];
+            var aRow = places[0, 1][0];
 
 
 
+            for (int i =0; i<places.Length;i++)
+            {
+                for(int j=0;j<places.Length;j++)
+                {
+                    for(int k=0;k<places.Length;k++)
+                    {
+                        a = places[i, j][k];
+                        aColumn = places[i, j][k + 1];
+                        aRow = places[i, j + 1][k];
+                        answer[i] = 0;
+                    }
+                }
 
-            int[] answer = new int[] { };
+            }
+            Console.WriteLine(a);
+
  
-            return answer;
+          //  return answer;
         }
     }
 }
